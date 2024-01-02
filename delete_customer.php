f6a760306ce27f24615d7d96dcf9667015d214d3
Rother7331 <90112047+Rother7331@users.noreply.github.com>
2024-01-02 19:09:30 +0100

    Add files via upload

---
+++ delete_customer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Usuń klienta</title>
+    <style>
+    </style>
+</head>
+
+<body>
+    <form action="add_customer.php" method="post">
+        <label for="imie">Imie: </label>
+            <input type="text" id="imie" name="imie"><br><br>
+        <label for="nazwisko">Nazwisko: </label>
+            <input type="text" id="nazwisko" name="nazwisko"><br><br>
+        <label for="adres">Adres zamieszkania: </label>
+            <input type="text" id="adres" name="adres"><br><br>
+        <label for="email">Email: </label>
+            <input type="text" id="email" name="email"><br><br>
+
+            <input type="submit" onclick="" value="USUN">
+
+    </form>
---
+++ delete_product.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Usuń klienta</title>
+    <style>
+    </style>
+</head>
+
+<body>
+    <form action="add_customer.php" method="post">
+        <label for="Nazwa">Nazwa: </label>
+            <input type="text" id="Nazwa" name="Nazwa"><br><br>
+        <label for="Opis">Opis: </label>
+            <input type="text" id="Opis" name="Opis"><br><br>
+        <label for="Ilosc">Ilosc: </label>
+            <input type="text" id="Ilosc" name="Ilosc"><br><br>
+        <label for="Dostepnosc">Dostepnosc: </label>
+            <input type="text" id="Dostepnosc" name="Dostepnosc"><br><br>
+
+            <input type="submit" onclick="" value="USUN">
+
+    </form>
